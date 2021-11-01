defa3c8e347382f4785ddaa01a09a560feacf517
EricLiv <ELivingston002@my.wilmu.edu>
2021-11-01 15:23:39 -0400

    init

---
+++ subappone/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Car
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Hello World')
+
+def about (request):
+    cars = Car.objects.all()
+    context = {
+        'car': cars
+    }
+    return render (request, 'subappone/about.html',{'cars':cars})
